bbf4a2eab53aba697b1e6a21a0015485d91cd75e
Tharaneesh Raj <tharaneeshsami@gmail.com>
2025-03-31 16:59:19 +0530

    Blur change commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
 import Register from './Components/register';
+import viewUsers from './Components/Users'
+import React ,{ useState } from 'react';
+
 
 function App() {
+
+  const [showUsers,setshowUsers] =useState(false)
   return (
-    <Register></Register>
-  );
+    <div>
+      {(showUsers)? null: <Register showUsers={showUsers}/>}
+      <viewUsers setshowUsers={setshowUsers}/>
+    </div>
+  )
 }
 
 export default App;
