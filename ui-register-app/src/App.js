d2b101a874f96db52bb64936e395336f8ff10135
Tharaneesh Raj <tharaneeshsami@gmail.com>
2025-03-31 10:20:02 +0530

    initial commit

---
+++ ui-register-app/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import Register from './Components/register';
+
+function App() {
+  return (
+    <Register></Register>
+  );
+}
+
+export default App;
---
+++ ui-register-app/src/Components/register.js
@@ -0,0 +1,10 @@
+
+
+const Register=()=>{
+    return(
+        <div>
+            Registration page
+        </div>
+    )
+}
+export default Register;
